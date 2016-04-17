c90add20ffed6c3dcbb4a8af1e4fbac7160a0e93
zjhmale <zjhmale@gmail.com>
2016-04-18 01:01:28 +0800

    print unicode

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,16 +6,17 @@ import Ast
 import Type
 import State
 import Infer
+import Text.Show.Unicode
 
-ttt :: Infer String
-ttt = do
+uni :: Infer T
+uni = do
     a <- newVar 0
     b <- newVar 0
     c <- newVar 0
     unify (TArrow [TConst "int", TConst "bool"] $ TConst "int") (TArrow [a, b] c)
-    return $ show a
+    return $ TArrow [a, b] c
 
 someFunc :: IO ()
 someFunc = do
-    t <- ttt
-    putStrLn $ "type -> " ++ t
+    t <- uni
+    uprint t
