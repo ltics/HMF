504d0d04017c60abeae358ec0e5a02fe7dbe2184
zjhmale <zjhmale@gmail.com>
2016-04-21 16:04:28 +0800

    finish FCP unification

--- src/FCP/Infer.hs
+++ src/FCP/Infer.hs
@@ -5,6 +5,8 @@ module FCP.Infer where
 import FCP.Type
 import State
 import Data.IORef
+import Control.Monad
+import Control.Exception
 import qualified Data.Map as M
 import qualified Data.Set as S
 
@@ -30,7 +32,7 @@ idTypeMapFrom2Lists :: [Id] -> [T] -> IdType
 idTypeMapFrom2Lists ids tys = foldl (\m (i, t) -> M.insert i t m) M.empty $ zip ids tys
 
 idTypeMapRemoveAll :: IdType -> [Id] -> IdType
-idTypeMapRemoveAll idTy ids = foldl (\m i -> M.delete i m) idTy ids
+idTypeMapRemoveAll = foldl $ flip M.delete
 
 occursCheckAdjustLevels :: Int -> Int -> T -> Infer ()
 occursCheckAdjustLevels tvId tvLevel t = case t of
@@ -79,7 +81,7 @@ substitudeBoundVars' idTy t = case t of
                                         _ -> return t
 
 substitudeBoundVars :: [Id] -> [T] -> T -> Infer T
-substitudeBoundVars varIds types t = substitudeBoundVars' (idTypeMapFrom2Lists varIds types) t
+substitudeBoundVars varIds types = substitudeBoundVars' (idTypeMapFrom2Lists varIds types)
 
 freeGenericVars :: T -> Infer (S.Set T)
 freeGenericVars t = do
@@ -102,3 +104,71 @@ freeGenericVars t = do
     f t
     freeVarSetV <- readIORef freeVarSet
     return freeVarSetV
+
+escapeCheck :: [T] -> T -> T -> Infer Bool
+escapeCheck genericVars t1 t2 = do
+    freeVars1 <- freeGenericVars t1
+    freeVars2 <- freeGenericVars t2
+    return $ any (\var -> S.member var freeVars1 || S.member var freeVars2) genericVars
+
+canNotUnifyError :: T -> T -> Infer ()
+canNotUnifyError t1 t2 = error $ "cannot unify types " ++ show t1 ++ " and " ++ show t2
+
+unify' :: T -> T -> Infer ()
+unify' (TConst name1) (TConst name2) | name1 == name2 = return ()
+unify' (TApp fn1 args1) (TApp fn2 args2) = do
+    unify fn1 fn2
+    zipWithM_ unify args1 args2
+unify' (TArrow params1 rtn1) (TArrow params2 rtn2) = do
+    zipWithM_ unify params1 params2
+    unify rtn1 rtn2
+unify' t1@(TVar ty1) t2@(TVar ty2) = do
+    ty1V <- readIORef ty1
+    ty2V <- readIORef ty2
+    case (ty1V, ty2V) of
+        (Unbound id1 level1, Unbound id2 _) -> if id1 == id2
+                                                then assert False return ()
+                                                else do
+                                                    occursCheckAdjustLevels id1 level1 t2
+                                                    writeIORef ty1 $ Link t2
+        (Bound _, _) -> assert False return ()
+        (_, Bound _) -> assert False return ()
+        (Unbound id1 level1, _) -> do
+            occursCheckAdjustLevels id1 level1 t2
+            writeIORef ty1 $ Link t2
+        (Link ty1', _) -> unify ty1' t2
+        (_, Unbound id2 level2) -> do
+            occursCheckAdjustLevels id2 level2 t1
+            writeIORef ty2 $ Link t1
+        (_, Link ty2') -> unify t1 ty2'
+        _ -> canNotUnifyError t1 t2
+unify' t1@(TVar ty1) ty2 = do
+    ty1V <- readIORef ty1
+    case ty1V of
+        Link ty1' -> unify ty1' ty2
+        Unbound id1 level1 -> do
+            occursCheckAdjustLevels id1 level1 ty2
+            writeIORef ty1 $ Link ty2
+        _ -> canNotUnifyError t1 ty2
+unify' ty1 t2@(TVar ty2) = do
+    ty2V <- readIORef ty2
+    case ty2V of
+        Link ty2' -> unify ty1 ty2'
+        Unbound id2 level2 -> do
+            occursCheckAdjustLevels id2 level2 ty1
+            writeIORef ty2 $ Link ty1
+        _ -> canNotUnifyError ty1 t2
+unify' ft1@(TForall varIds1 t1) ft2@(TForall varIds2 t2) = if length varIds1 /= length varIds2
+                                                           then canNotUnifyError t1 t2
+                                                           else do
+                                                            genericVars <- mapM (const newGenVar) varIds1
+                                                            let genericVars' = reverse genericVars
+                                                            genericTy1 <- substitudeBoundVars varIds1 genericVars' t1
+                                                            genericTy2 <- substitudeBoundVars varIds2 genericVars' t2
+                                                            unify genericTy1 genericTy2
+                                                            isEscape <- escapeCheck genericVars' ft1 ft2
+                                                            when isEscape $ canNotUnifyError ft1 ft2
+unify' ty1 ty2 = canNotUnifyError ty1 ty2
+
+unify :: T -> T -> Infer ()
+unify ty1 ty2 = unless (ty1 == ty2) $ unify' ty1 ty2
