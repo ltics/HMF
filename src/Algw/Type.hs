829d73b815ffcc4c51285cc878a3fd6d3a169fb9
zjhmale <zjhmale@gmail.com>
2016-04-23 23:31:02 +0800

    finish basic ast and type for classic algw

--- src/Algw/Type.hs
+++ src/Algw/Type.hs
@@ -1,6 +1,13 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Algw.Type where
+
+import Algw.Ast
+import Data.Maybe
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 type TName = String
 
@@ -17,6 +24,56 @@ instance Show T where
   show (TArrow t1@(TArrow _ _) t2) = "(" ++ show t1 ++ ")" ++ " → " ++ show t2
   show (TArrow t1 t2) = show t1 ++ " → " ++ show t2
 
-data Scheme = Mono T
-            | Poly TName Scheme
+-- principle type schemes
+data Scheme = Mono T --monomorphic types
+            | Poly TName Scheme -- polymorphic types
   deriving (Eq, Ord)
+
+instance Show Scheme where
+  show (Mono t) = show t
+  show (Poly a t) = "∀" ++ a ++ ". " ++ show t
+
+-- a substitution rule is just mapping type variables to mono types
+type Subrule = M.Map TName T
+
+emptyRule :: Subrule
+emptyRule = M.empty
+
+-- subrule composition s2 ∘ s1 t = s2 (s1 t)
+compose :: Subrule -> Subrule -> Subrule
+compose s s' = M.map (subst s) s' `M.union` s
+
+class TypeVars a where
+  allVars :: a -> S.Set TName
+  freeVars :: a -> S.Set TName
+  subst :: Subrule -> a -> a
+
+instance TypeVars T where
+  allVars (TVar a) = S.singleton a
+  allVars (TArrow t1 t2) = allVars t1 `S.union` allVars t2
+  allVars _ = S.empty
+
+  freeVars = allVars
+
+  -- use default value
+  subst s v@(TVar n) = fromMaybe v $ M.lookup n s
+  subst s (TArrow t1 t2) = subst s t1 `TArrow` subst s t2
+  subst _ t = t
+
+instance TypeVars Scheme where
+  allVars (Mono t) = allVars t
+  allVars (Poly a t) = S.insert a $ allVars t
+
+  freeVars (Mono t) = freeVars t
+  freeVars (Poly a t) = S.delete a $ allVars t
+
+  subst s (Mono t) = Mono $ subst s t
+  subst s (Poly a t) = Poly a $ subst (M.delete a s) t
+
+-- an environment is mapping variables to type schemes
+type Env = M.Map EName Scheme
+
+instance TypeVars Env where
+  allVars = M.foldl (\avs t -> S.union avs $ allVars t) S.empty
+  freeVars = M.foldl (\fvs t -> S.union fvs $ freeVars t) S.empty
+  subst s = M.map (subst s)
